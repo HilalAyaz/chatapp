6917674b3e29c75aa4abf80d36a7ce80d3c9aa5e
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-02-22 16:57:00 +0500

    .

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
-  const [username, setUsername] = useState(""); 
-  const [password, setPassword] = useState(""); 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const { loading, login } = useLogin();
 
@@ -16,7 +16,7 @@ const Login = () => {
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login <span className="text-blue-500">ChatApp</span>
+          Login <span className="text-blue-500">ChatZilla</span>
         </h1>
         <form onSubmit={handleSubmit}>
           <div>
