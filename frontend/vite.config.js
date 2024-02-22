5064211be0f62edc8f1ae6775ba70f55ca900d9f
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-02-22 17:07:06 +0500

    ..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'chatapp-backend-devdesigner.vercel.app'
     }
   }
 })
