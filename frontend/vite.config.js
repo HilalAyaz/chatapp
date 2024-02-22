267ac083a98e796d1d2bdcf0ac739199eaaf385c
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-02-22 16:31:31 +0500

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,5 +8,8 @@ export default defineConfig({
     proxy:{
       "/api": "http://localhost:5000"
     }
-  }
-})
+  },
+  optimizeDeps: {
+    exclude: ['socket.io-client'],
+  },
+});
