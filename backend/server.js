d5cd133a0cea875bd9a8cde4a6475b5dfac2b4e6
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-02-22 10:35:31 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
@@ -6,12 +7,12 @@ import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
-
 import connectToMongoDB from './db/connectToMongodb.js'
 import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
 
 dotenv.config()
 
@@ -22,9 +23,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
 
-// app.get('/', (req, res) => {
-//   res.send('hello world!')
-// })
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 server.listen(PORT, () => {
   connectToMongoDB()
